4fcdde40278fe940b185bef7f70209386ab99976
SanketPaithankar <156290564+SanketPaithankar@users.noreply.github.com>
2025-03-09 18:55:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,6 @@
+import streamlit as st
+# Make Predictions
+# Use the selected model to make predictions based on user input and display the results on the screen.
 
+# Streamlit app
+st.title('Weather Prediction')
