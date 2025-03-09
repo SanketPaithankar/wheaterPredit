223e2b88f5295ae3fb1c7e807c68576be1e0480c
SanketPaithankar <156290564+SanketPaithankar@users.noreply.github.com>
2025-03-09 19:06:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import streamlit as st
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.linear_model import LinearRegression
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import mean_squared_error, accuracy_score
+
 # Make Predictions
 # Use the selected model to make predictions based on user input and display the results on the screen.
 
